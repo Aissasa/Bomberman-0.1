1f002269d34232410be6e79279beae804ea7a71d
FIEA\abenzayed <aissa.ben.zayed@gmail.com>
2016-11-30 16:39:41 -0500

    Initial commit

---
+++ Bomberman/Game/Header Files/baseTypes.h
@@ -0,0 +1,14 @@
+typedef struct 
+{
+    float x;
+    float y;
+}Coord2D;
+
+typedef char bool8_t;
+typedef char char8_t;
+typedef unsigned char uchar8_t;
+typedef short int16_t;
+typedef unsigned short uint16_t;
+typedef int int32_t;
+typedef unsigned int uint32_t;
+typedef float float_t;
